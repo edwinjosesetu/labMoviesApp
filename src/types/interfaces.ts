af8d88050ce4bda49fd01050ca712590d85a4475
edwinjosesetu <20110463@mail.wit.ie>
2025-04-26 15:11:46 +0100

    Movie Details Page - included production countries.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,43 +1,49 @@
 export interface BaseMovieProps {
-    title: string;
-    budget: number;
-    homepage: string | undefined;
-    id: number;
-    imdb_id: string;
-    original_language: string;
-    overview: string;
-    release_date: string;
-    vote_average: number;
-    popularity: number;
-    poster_path?: string;
-    tagline: string;
-    runtime: number;
-    revenue: number;
-    vote_count: number;
-    favourite?: boolean;
-  }
-export interface BaseMovieListProps { 
-    movies: BaseMovieProps[];
-  }
-  
-export interface MovieDetailsProps extends BaseMovieProps {
-    genres: {
-      id: number;
-      name: string;
-    }[];
-  }
+  title: string;
+  budget: number;
+  homepage: string | undefined;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  overview: string;
+  release_date: string;
+  vote_average: number;
+  popularity: number;
+  poster_path?: string;
+  tagline: string;
+  runtime: number;
+  revenue: number;
+  vote_count: number;
+  favourite?: boolean;
+}
+export interface BaseMovieListProps {
+  movies: BaseMovieProps[];
+}
 
-  export interface MovieImage {
-    file_path: string;
-    aspect_ratio?: number; //some props are optional...
-    height?: number;
-    iso_639_1?: string;
-    vote_average?: number;
-    vote_count?: number;
-    width?: number;
-  }
-  
-  export interface MoviePageProps {
-    movie: MovieDetailsProps;
-    images: MovieImage[];
-  }  
+export interface MovieDetailsProps extends BaseMovieProps {
+  genres: {
+    id: number;
+    name: string;
+  }[];
+
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];  
+}
+
+export interface MovieImage {
+  file_path: string;
+  aspect_ratio?: number; //some props are optional...
+  height?: number;
+  iso_639_1?: string;
+  vote_average?: number;
+  vote_count?: number;
+  width?: number;
+}
+
+export interface MoviePageProps {
+  movie: MovieDetailsProps;
+  images: MovieImage[];
+}
+
