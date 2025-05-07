b00a362a7be0e980508ad952f8b96a1ca2d34396
edwinjosesetu <20110463@mail.wit.ie>
2025-05-07 19:38:58 +0100

    Add popular movies page

--- src/pages/popularMoviesPage.tsx
+++ src/pages/popularMoviesPage.tsx
@@ -1,28 +1,34 @@
-import { useState, useEffect } from "react";
-import { getPopularMovies } from "../api/tmdb-api";
+import React from "react";
 import PageTemplate from "../components/templateMovieListPage";
-import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
+import { getPopularMovies } from "../api/tmdb-api";
 import { BaseMovieProps } from "../types/interfaces";
+import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
+import { useQuery } from "react-query";
+import Spinner from "../components/spinner";
 
-const PopularMoviesPage = () => {
-    const [movies, setMovies] = useState<BaseMovieProps[]>([]);
+const PopularMoviesPage: React.FC = () => {
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["popularMovies"],
+    queryFn: getPopularMovies,
+  });
 
+  if (isLoading) {
+    return <Spinner />;
+  }
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const movies = await getPopularMovies();
-      setMovies(movies);
-    };
-    fetchData();
-  }, []);
+  if (isError) {
+    return <h1>{(error as Error).message}</h1>;
+  }
 
   return (
     <PageTemplate
-          title="Popular Movies"
-          movies={movies}
-          action={(movie: BaseMovieProps) => <AddToFavouritesIcon {...movie} />} selectFavourite={function (movieId: number): void {
-              throw new Error("Function not implemented.");
-          } }    />
+      title="Popular Movies"
+      movies={data ?? []}
+      selectFavourite={() => {
+        throw new Error("Function not implemented.");
+      }}
+      action={(movie: BaseMovieProps) => <AddToFavouritesIcon {...movie} />}
+    />
   );
 };
 
